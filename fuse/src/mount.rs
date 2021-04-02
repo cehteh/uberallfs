c3ec4fd1fccd290dbea320400e3e366ae2c65bf0
Christian Thäter <ct@pipapo.org>
2021-04-02 19:28:17 +0200

    more in fuse implementaton, ready to start the Filesystem impl now

--- fuse/src/mount.rs
+++ fuse/src/mount.rs
@@ -15,23 +15,21 @@ use crate::uberallfs::UberallFS;
 
 pub(crate) fn opt_mount(mountpoint: &OsStr, matches: &ArgMatches) -> Result<()> {
     trace!("mountpoint: {:?}", mountpoint);
-    let objectstore_dir = matches
-        .value_of_os("OBJECTSTORE")
-        .or(Some(mountpoint))
-        .unwrap();
+    let objectstore_dir = Path::new(
+        matches
+            .value_of_os("OBJECTSTORE")
+            .or(Some(mountpoint))
+            .unwrap(),
+    );
+
+    let mountpoint = Path::new(mountpoint);
 
     trace!("objectstore: {:?}", objectstore_dir);
-    let mut objectstore = ObjectStore::open(Path::new(objectstore_dir))?;
 
-    let offline = matches.is_present("offline");
-
-    let mut options = vec![
-        MountOption::RO,
-        MountOption::FSName("uberallfs".to_string()),
-    ];
-    options.push(MountOption::AutoUnmount); //TODO: optarg?
-
-    fuser::mount2(UberallFS, mountpoint, &options);
-
-    Ok(())
+    UberallFS::new(objectstore_dir)?.mount(
+        mountpoint,
+        matches.is_present("offline"),
+        matches.value_of_os("root"),
+        None,
+    )
 }
--- fuse/src/uberallfs.rs
+++ fuse/src/uberallfs.rs
@@ -1,10 +1,46 @@
 use crate::prelude::*;
 
+use std::path::Path;
+
+use std::ffi::OsStr;
+
+use objectstore::objectstore::ObjectStore;
+
 use fuser::{
     FileAttr, FileType, Filesystem, MountOption, ReplyAttr, ReplyData, ReplyDirectory, ReplyEntry,
     Request,
 };
 
-pub struct UberallFS;
+pub struct UberallFS {
+    objectstore: ObjectStore,
+}
 
-impl Filesystem for UberallFS {}
+impl UberallFS {
+    pub fn new(objectstore_dir: &Path) -> Result<UberallFS> {
+        Ok(UberallFS {
+            objectstore: ObjectStore::open(objectstore_dir)?,
+        })
+    }
+
+    pub fn mount(
+        self,
+        mountpoint: &Path,
+        offline: bool,
+        root: Option<&OsStr>,
+        _options_planned: Option<Vec<String>>,
+    ) -> Result<()> {
+        let mut options = vec![
+            MountOption::RO,
+            MountOption::FSName("uberallfs".to_string()),
+        ];
+        options.push(MountOption::AutoUnmount); //TODO: optarg?
+        fuser::mount2(self, mountpoint, &options)?;
+        Ok(())
+    }
+}
+
+impl Filesystem for UberallFS {
+
+
+
+}
